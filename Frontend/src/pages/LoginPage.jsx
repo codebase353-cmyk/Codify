798ab5d9f84a180e251c3f5d21109e2b3553c56e
ansh766 <rastogiiansh@gmail.com>
2026-03-26 21:35:10 +0530

    https://github.com/codebase353-cmyk/Codify.git

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -44,7 +44,7 @@ export const LoginPage = () => {
         <div className="flex flex-col items-center">
           <HackForgeLogo size={8} />
           <h2 className="mt-3 text-center text-3xl font-extrabold text-slate-900 dark:text-white">
-            Welcome Back to HackForge
+            Welcome Back to CodifyX
           </h2>
           <p className="mt-2 text-center text-sm text-slate-600 dark:text-gray-400">
             Log in to continue your coding journey.
